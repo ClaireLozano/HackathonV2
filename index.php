fe6f8ee31438864c7924f495d3feb78e6927051c
Claire Lozano <claire.lozano@live.fr>
2018-01-30 13:22:56 +0100

    réparation route (navbar && index.php)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>OpenData La Rochelle</title>
-		<meta http-equiv="refresh" content="0; URL=components/home/index.php">
+		<meta http-equiv="refresh" content="0; URL=js/components/home/index.php">
 	</head>
 	 
 	<body>
