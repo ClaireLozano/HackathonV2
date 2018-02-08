8606e2017c6349a0cff60173060c98c0b2343ed0
Claire Lozano <claire.lozano@live.fr>
2018-02-08 14:34:50 +0100

    Tableau INSEE dictionnary POP && INSEE OK

--- js/components/util.js
+++ js/components/util.js
@@ -93,7 +93,7 @@ var reloadPage = function(url, endUrl, typeVisualisation) {
  *
  * @param  {Object}         data                Data from open data la rochelle plateform
  * @param  {Object}         metadata            Meta data
- * @param  {Number}         position            Position of random box
+ * @param  {Number}         idBox               Id of the box where the table will be
  *
  * @return null
  */
@@ -105,125 +105,167 @@ function drawTable(data, metadata, idBox) {
         return;
     }
 
-    // Initialization
-    var keys_list_table = [];
-    var value_list_table = [];
+    // Create table and insert title row on table 
+    insertRowTitle(data, metadata, idBox, function(err) {
+        // Insert rows on table
+        insertRows(data, metadata, idBox, function(err) {
+            // Draw table with DataTable
+            setTable(idBox);
+        });
+    });
+
     
-    keys_list_table = metadata.table.dataComposition.keys_list;
+};
+
+/**
+ * Create table and th
+ *
+ * @return 
+ */
+var insertRowTitle = function(data, metadata, idBox, callback) {
+
+    // Initialization
+    var keys_list_table = metadata.table.dataComposition.keys_list;
+
+    // Create table
+    var val_html = "<table id='my_table_" + idBox + "' class='table table-list-search table-striped table-bordered' cellspacing='0' width='100%'><thead><tr>";
 
     // Get dictionnary 
-    if (metadata.dictionnaire) {
-        urlDict = metadata.dictionnaire[0];
-        initValue = metadata.dictionnaire[1];
-        newValue = metadata.dictionnaire[2];
+    if (metadata.dictionnaireX) {
+        var urlDict = metadata.dictionnaireX.link;
+        var initValue = metadata.dictionnaireX.initValue;
+        var newValue = metadata.dictionnaireX.newValue;
 
+        // Get dictionnary
         getData(urlDict, function(dict) {
-            // Create table
-            val_html = "<table id='my_table_" + idBox + "' class='table table-list-search table-striped table-bordered' cellspacing='0' width='100%'><thead><tr>";
+
+            // Initialize
             var found = false;
+
             // Create header table
-            keys_list_table.forEach(function(key, j) {
+            keys_list_table.forEach(function(key, j, arrJ) {
                 found = false;
                 dict.forEach(function(d, i, arr) {
+
                     // If element is on the dictionnary, take the dictionnary value
                     if (d[initValue] === key) {
                         val_html += "<th>" + d[newValue] + "</th>";
                         found = true;
-                        return;
                     }
+
                     // If the element isn't found in the dictionnary, keep the keys_list_table value
                     if (i === arr.length - 1 && !found) { 
-                       val_html += "<th>" + key + "</th>";
-                    }
-                });
-            });
-            
-            val_html += "</tr></thead><tbody id='table_element_" + idBox + "'></tbody></table>";
+                        val_html += "<th>" + key + "</th>";
 
-            // Insert Row
-            document.getElementById(idBox).innerHTML = val_html;
-            console.log(data)
-            data.forEach(function(d, i) {
-                var table = document.getElementById('my_table_' + idBox);
-                var tr = table.insertRow(i);
-                keys_list_table.forEach(function(p, j) {
-                    tr.insertCell(j).innerHTML = d[p];
-                    document.getElementById("table_element_" + idBox).appendChild(tr);     
+                        // Last iteration, return
+                        if (j === arrJ.length - 1) { 
+                            val_html += "</tr></thead><tbody id='table_element_" + idBox + "'></tbody></table>";
+                            document.getElementById(idBox).innerHTML = val_html;
+                            return callback();
+                        }
+                    }
+
+                    // If last element, return
+                    if (i === arr.length - 1 && j === arrJ.length - 1) { 
+                        val_html += "</tr></thead><tbody id='table_element_" + idBox + "'></tbody></table>";
+                        document.getElementById(idBox).innerHTML = val_html;
+                        return callback();
+                    }
                 });
             });
-            $('#my_table_' + idBox).DataTable({
-                "language": {
-                    "sProcessing":     "Traitement en cours...",
-                    "sSearch":         "Rechercher&nbsp;:",
-                    "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
-                    "sInfo":           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-                    "sInfoEmpty":      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-                    "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-                    "sInfoPostFix":    "",
-                    "sLoadingRecords": "Chargement en cours...",
-                    "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-                    "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
-                    "oPaginate": {
-                        "sFirst":      "Premier",
-                        "sPrevious":   "←",
-                        "sNext":       "→",
-                        "sLast":       "Dernier"
-                    },
-                    "oAria": {
-                        "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
-                        "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
-                    }
-                }
-            });
-            setTable();
         });
     } else {
-        value_list_table = metadata.table.dataComposition.value_list;
-        
-        // Create table
-        val_html = "<table id='my_table_" + idBox + "' class='table table-list-search table-striped table-bordered' cellspacing='0' width='100%'><thead><tr>";
+
+        // Get title from metadata
+        var value_list_table = metadata.table.dataComposition.value_list;
 
         // Create header table
-        value_list_table.forEach(function(d) {
+        value_list_table.forEach(function(d, i, arr) {
             val_html += "<th>" + d + "</th>"
-        });
-        val_html += "</tr></thead><tbody id='table_element_" + idBox + "'></tbody></table>";
 
-        // Insert Row
-        document.getElementById(idBox).innerHTML = val_html;
-        data.forEach(function(d, i) {
+            // Last iteration, return
+            if (i === arr.length - 1) { 
+                val_html += "</tr></thead><tbody id='table_element_" + idBox + "'></tbody></table>";
+                document.getElementById(idBox).innerHTML = val_html;
+                return callback();
+            }
+        });
+    }
+};
+
+/**
+ * Insert rows
+ *
+ * @return 
+ */
+var insertRows = function(data, metadata, idBox, callback) {
+
+    // Initialization
+    var keys_list_table = metadata.table.dataComposition.keys_list; 
+    
+    // Insert Rows
+    if (metadata.dictionnaireY) {
+        var urlDict = metadata.dictionnaireY.link;
+        var value = metadata.dictionnaireY.value;
+        var initValue = metadata.dictionnaireY.initValue;
+        var newValue = metadata.dictionnaireY.newValue;
+
+        // Get dictionnary
+        getData(urlDict, function(dict) {
+            data.forEach(function(d, i, arrI) {
+                var table = document.getElementById('my_table_' + idBox);
+                var tr = table.insertRow(i);
+                keys_list_table.forEach(function(key, j, arrJ) {
+                    insertValue = d[key];
+                    var found = false;
+                    if (key === value) {
+                        dict.forEach(function(row, k, arr) {
+
+                            // If element is on the dictionnary, take the dictionnary value
+                            if (d[key] === row[value]) {
+                                insertValue = row[newValue];
+                                tr.insertCell(j).innerHTML = insertValue;
+                                document.getElementById("table_element_" + idBox).appendChild(tr); 
+                                found = true;
+                            } 
+
+                            // If the element isn't found in the dictionnary, keep the keys_list_table value
+                            if (k === arr.length - 1 && !found) { 
+                                tr.insertCell(j).innerHTML = insertValue;
+                                document.getElementById("table_element_" + idBox).appendChild(tr); 
+
+                                // Return if last element
+                                if (j === arrJ.length - 1 && i === arrI.length - 1) {
+                                    return callback();
+                                } 
+                            }
+                        });
+                    } else {
+                        tr.insertCell(j).innerHTML = insertValue;
+                        document.getElementById("table_element_" + idBox).appendChild(tr); 
+
+                        // Return if last element
+                        if (j === arrJ.length - 1 && i === arrI.length - 1) {
+                            return callback();
+                        } 
+                    }
+                });
+            });
+        });
+    } else {
+        data.forEach(function(d, i, arr) {
             var table = document.getElementById('my_table_' + idBox);
             var tr = table.insertRow(i);
-            keys_list_table.forEach(function(p, j) {
-                tr.insertCell(j).innerHTML = d[p];
-                document.getElementById("table_element_" + idBox).appendChild(tr);     
+            keys_list_table.forEach(function(key, j, arrJ) {
+                tr.insertCell(j).innerHTML = d[key];
+                document.getElementById("table_element_" + idBox).appendChild(tr); 
+                
+                // Return if last element
+                if (i === arr.length - 1 && j === arrJ.length - 1) {
+                    return callback();
+                }    
             });
-        }); 
-        $('#my_table_' + idBox).DataTable({
-                "language": {
-                    "sProcessing":     "Traitement en cours...",
-                    "sSearch":         "Rechercher&nbsp;:",
-                    "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
-                    "sInfo":           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-                    "sInfoEmpty":      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
-                    "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
-                    "sInfoPostFix":    "",
-                    "sLoadingRecords": "Chargement en cours...",
-                    "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
-                    "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
-                    "oPaginate": {
-                        "sFirst":      "Premier",
-                        "sPrevious":   "←",
-                        "sNext":       "→",
-                        "sLast":       "Dernier"
-                    },
-                    "oAria": {
-                        "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
-                        "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
-                    }
-                }
-            });
-        setTable();
+        });
     }
 };
 
@@ -232,7 +274,32 @@ function drawTable(data, metadata, idBox) {
  *
  * @return type if visualisation 
  */
-var setTable = function() {
+var setTable = function(idBox) {
+    $('#my_table_' + idBox).DataTable({
+        "language": {
+            "sProcessing":     "Traitement en cours...",
+            "sSearch":         "Rechercher&nbsp;:",
+            "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
+            "sInfo":           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            "sInfoEmpty":      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+            "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+            "sInfoPostFix":    "",
+            "sLoadingRecords": "Chargement en cours...",
+            "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
+            "oPaginate": {
+                "sFirst":      "Premier",
+                "sPrevious":   "←",
+                "sNext":       "→",
+                "sLast":       "Dernier"
+            },
+            "oAria": {
+                "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+                "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+            }
+        }
+    });
+
     $(".dataTables_info").remove();
     // $("#my_table_box1_filter label").html("Rechercher: ");
 };
