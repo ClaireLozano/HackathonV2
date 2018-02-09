9f0cd8016d98ef699af80c3ddcf85e2d6cb0da7d
Claire Lozano <claire.lozano@live.fr>
2018-02-09 22:41:19 +0100

    CSS

--- js/components/home/index.php
+++ js/components/home/index.php
@@ -7,7 +7,7 @@
 
 		<div id="welcome" class="box-wrapper-inner">
 			<div class="panel panel-default">
-		        <div class="panel-body">
+		        <div id="panel-welcome">
 		            <h3>Bienvenue sur Opendata La Rochelle </h3>
 		            </br>
 		            <p>le site de la démarche Open Data de la Ville de La Rochelle. Vous trouverez ici l'ensemble des jeux de données publiés par les services de la Ville et ses partenaires sous plusieurs forme de visualisation : cartographique, graphique, tableau ainsi que line temporelle !</p>
