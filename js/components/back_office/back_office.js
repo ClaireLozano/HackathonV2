20f2791215e7ced046b1c122008421f234a018a0
Claire Lozano <claire.lozano@live.fr>
2018-02-15 15:05:00 +0100

    css

--- js/components/back_office/back_office.js
+++ js/components/back_office/back_office.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  function init() {
 
-  }
-  init();
+	function init() {
+	
+	}
+	
+ 	init();
 });
 
 function addInput(){
-  var inputDiv = document.getElementById('keyListInput');
-  var newInput = document.createElement("INPUT");
-  newInput.setAttribute("type", "text");
-  newInput.setAttribute("name", "table_dataComposition_keys_list[]");
-  newInput.setAttribute("class","form-control");
-  inputDiv.appendChild(newInput);
-}
+	var inputDiv = document.getElementById('keyListInput');
+	var newInput = document.createElement("INPUT");
+	newInput.setAttribute("type", "text");
+	newInput.setAttribute("name", "table_dataComposition_keys_list[]");
+	newInput.setAttribute("class","form-control");
+	inputDiv.appendChild(newInput);
+};
--- js/components/back_office/back_office.php
+++ js/components/back_office/back_office.php
@@ -8,28 +8,42 @@
     <div id="content-page">
       <h2>Back office</h2>
       <p>Création de métadonnes pour les jeux de données </p>
-      <form action="postdata.php" method="post">
+      <form id="form" action="postdata.php" method="post">
 
         <div class="form-row">
           <hr>
           <h3>Détails jeu de données</h3>
           <div class="form-group col-md-10">
+            <label for="inputTitleData">Titre de la données</label>
+            <p>Correspond au titre à afficher sur la page de visualisation (Ex: "BUDGET PRIMITIF ET COMPTE ADMINISTRATIF - BP 2017 - FONCTION"</p>
+            <input type="text" class="form-control" name="inputTitleData">
+          </div>
+          <div class="form-group col-md-10">
+            <label for="inputDescriptionData">Lien vers les données</label>
+            <p>Il est possible d'afficher une description de la données et ainsi guider les utilisateurs dans la lecture des données.</p>
+            <textarea form="form" type="text" class="form-control" name="inputDescriptionData"></textarea>
+          </div>
+          <div class="form-group col-md-10">
             <label for="inputLink">Nom du fichier métadonnées</label>
-            <h6>Ex: "parking", "insee" etc </h6>
+            <p>Le fichier doit prendre le nom de l'identifiant de la données (Ex: "menu_allergenes_accomp")</p>
             <input type="text" class="form-control" name="fileName">
           </div>
           <div class="form-group col-md-10">
-            <label for="inputDataType">Type de données :</label>
-            <div class="form-check">
-              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="NotHistorisedLocalisable">Non-historisable et localisable</label>
-              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="HistorisedNotLocalisable">Historisable et non-localisable</label>
-              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="HistorisedLocalisable">Historisable et localisable</label>
-              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="NotHistorisedNotLocalisable">Non-historisable et non-localisable</label>
-            </div>
+            <label for="inputLink">Lien vers les données</label>
+            <p>Ce bout de lien sert à faire la relation entre la plateforme et le service web. (Ex: "&db=budget&table=bp_2014_fonction&format=json")</p>
+            <input type="text" class="form-control" name="link">
           </div>
           <div class="form-group col-md-10">
-            <label for="inputLink">Lien vers les données</label>
-            <input type="text" class="form-control" name="link">
+            <label for="inputDataType">Type de données :</label>
+            <p>Les données de l'open data peuvent être divers et varié. Elles peuvent être historisées, c'est-à-dire qu'une données a pu être enregistré à plusieurs moment dans le temps (Ex: bugdet 2015 et budget 2016) et elle peuvent être de type géographique et donc représentable sur une carte. On a finallement retenue 5 types de données qui sont : </p>
+            <p>Todo : mettre une select list à la place (?)</p>
+            <div class="form-check">
+              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="NotHistorisedLocalisable"> Non-historisable et localisable</label>
+              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="HistorisedNotLocalisable"> Historisable et non-localisable</label>
+              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="HistorisedLocalisable"> Historisable et localisable</label>
+              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="NotHistorisedNotLocalisable"> Non-historisable et non-localisable</label>
+              <label class="form-check-label"><input class="form-check-input" type="radio" name="datatype" value="NotHistorisedNotLocalisable"> Localisable</label>
+            </div>
           </div>
         </div>
 
@@ -37,6 +51,7 @@
           <hr>
           <h3>Détails du graphe</h3>
           <label for="inputGraphType">Type de graphe : </label>
+          <p>Un graphe peut-être représenté sous plusieurs formes. Veuillez choisir le type de réprésentation :</p>
           <div class="form-check">
             <label class="form-check-input"><input type="checkbox" name="possibleGraphs[]" value="pie">Camembert</label>
             <label class="form-check-input"><input type="checkbox" name="possibleGraphs[]" value="bar">Barres</label>
@@ -67,17 +82,16 @@
         <div class="form-group col-md-10">
           <hr>
           <h3>Détails du tableau</h3>
-          <label for="inputTableTitle">Titre du tableau</label>
-          <input type="text" class="form-control" name="table_dataComposition_title">
-        </div>
-
-        <div class="form-row">
-          <div id="keyListInput" class="form-group col-md-10">
-            <label for="inputKeyList">Liste des mots clés</label>
-            <input type="text" class="form-control" name="table_dataComposition_keys_list[]">
-          </div>
-          <div id="keyListInput" class="form-group col-md-3">
-            <input type="button" value="Ajouter un mot clé" onclick="addInput()">
+          <div class="form-row">
+            <div id="keyListInput" class="form-group col-md-10">
+              <label for="inputKeyList">Liste des titres des colones</label>
+              <p>Dans un premier temps, veuillez lister toutes les colonnes à afficher ainsi que la valeur à afficher sur l'interface utilisateur.</p>
+              <p>Todo : mettre des input l'un a coté de l'autre pour entré la variable et le nom à afficher (genre pb_paking --> place de parking) et mettre une croix pour supprimer une ligne</p>
+              <input type="text" class="form-control" name="table_dataComposition_keys_list[]">
+            </div>
+            <div id="keyListInput" class="form-group col-md-3">
+              <input type="button" value="Ajouter un mot clé" onclick="addInput()">
+            </div>
           </div>
         </div>
 
