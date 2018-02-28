f6bf3ce5b797b816a4965989247f69484595a45d
quentinncl <qnicolle@me.com>
2018-02-28 23:55:25 +0100

    fixing display charts on home

--- js/components/home/index.php
+++ js/components/home/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-	
+
 	<?php include '../header/header.php'; ?>
-	
+
 	<body>
 
 		<div id="welcome" class="box-wrapper-inner">
@@ -17,12 +17,12 @@
 		    </div>
 		</div>
 
-		<!-- Première box --> 
+		<!-- Première box -->
 		<div class="box-wrapper-inner">
             <div class="panel panel-default box">
                 <div class="panel-heading">
                     <p id="panel-title-box0" class="panel-title pull-left"></p>
-                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !"> 
+                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !">
                         <button class="btn btn-default label label-warning pull-right"> Un problème ?</button>
                     </a>
                     <div class="clearfix"></div>
@@ -40,10 +40,10 @@
             <div class="panel panel-default box">
                 <div class="panel-heading">
                     <p id="panel-title-box1" class="panel-title pull-left"></p>
-                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !"> 
+                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !">
                         <button class="btn btn-default label label-warning pull-right"> Un problème ?</button>
                     </a>
-                    
+
                     <div class="clearfix"></div>
                 </div>
                 <div id='box1' class="panel-body form-group panelBody">
@@ -59,7 +59,7 @@
             <div class="panel panel-default box">
                 <div class="panel-heading">
                     <p id="panel-title-box2" class="panel-title pull-left"></p>
-                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !"> 
+                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !">
                         <button class="btn btn-default label label-warning pull-right"> Un problème ?</button>
                     </a>
 
@@ -78,10 +78,10 @@
             <div class="panel panel-default box">
                 <div class="panel-heading">
                     <p id="panel-title-box3" class="panel-title pull-left"></p>
-                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !"> 
+                    <a href="mailto:claire.lozano@live.fr?subject=Problème sur la plateforme&body=Les données vous semble incorrectes ? La visualisationne vous convient pas ? Aidez-nous à améliorer la plateforme !">
                         <button class="btn btn-default label label-warning pull-right"> Un problème ?</button>
                     </a>
-                    
+
                     <div class="clearfix"></div>
                 </div>
                 <div id='box3' class="panel-body form-group panelBody">
@@ -101,12 +101,14 @@
     <script type="text/javascript" src="../visualisation/map/poste.js"></script>
     <script type="text/javascript" src="../visualisation/map/geoloc.js"></script>
     <script type="text/javascript" src="../visualisation/map/map.js"></script>
-    <script type="text/javascript" src="../visualisation/chart/chart.js"></script>
-    <script type="text/javascript" src="../visualisation/table/table.js"></script>
+		<script type="text/javascript" src="../visualisation/chart/bar.js"></script>
+		<script type="text/javascript" src="../visualisation/chart/horizontalBar.js"></script>
+		<script type="text/javascript" src="../visualisation/chart/pie.js"></script>
+		<script type="text/javascript" src="../visualisation/chart/doughnut.js"></script>
+		<script type="text/javascript" src="../visualisation/chart/chart.js"></script>
+		<script type="text/javascript" src="../visualisation/table/table.js"></script>
     <script type="text/javascript" src="../controller.js"></script>
     <script type="text/javascript" src="../util.js"></script>
     <script type="text/javascript" src="index.js"></script>
 
 </html>
-
-
