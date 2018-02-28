c0f4c84bb874e698518cf1b1f871a52eb5a49e9c
quentinncl <qnicolle@me.com>
2018-02-28 23:55:25 +0100

    fixing display charts on home

--- js/components/visualisation/chart/chart.js
+++ js/components/visualisation/chart/chart.js
@@ -6,45 +6,50 @@
  */
 function drawGraph(dataToTreat, metadata, box) {
 
-    var select = d3.select('#' + box)
-        .append('select')
-        .classed('selectGraph', true)
-        .on('change', onChange)
+    if(metadata.graph.dataComposition.category0){
+      initPie(dataToTreat, metadata, box)
 
-    var options = select.selectAll('option')
-        .data(metadata.graph.possibleGraphs).enter()
-        .append('option')
-        .attr("value", function (d) {
-            return d
-        })
-        .text(function (d) {
-            return d
-        })
+    } else {
+      var select = d3.select('#' + box)
+      .append('select')
+      .classed('selectGraph', true)
+      .on('change', onChange)
 
-    function onChange() {
+      var options = select.selectAll('option')
+      .data(metadata.graph.possibleGraphs).enter()
+      .append('option')
+      .attr("value", function (d) {
+        return d
+      })
+      .text(function (d) {
+        return d
+      })
+
+      function onChange() {
         var selectValue = d3.select('.selectGraph').property('value')
         console.log(selectValue)
         switch (selectValue) {
-            case "bar":
-                initBar(dataToTreat, metadata, box)
-                break
-            case "pie":
-                initPie(dataToTreat, metadata, box)
-                break
-            case "doughnut":
-                initdoughnut(dataToTreat, metadata, box)
-                break
-            case "horizontalBar":
-                initHorizontalBar(dataToTreat, metadata, box)
-                break
-            default:
-                d3.selectAll("svg").remove()
-                d3.selectAll(".chart>p").remove()
+          case "bar":
+            initBar(dataToTreat, metadata, box)
+            break
+          case "pie":
+            initPie(dataToTreat, metadata, box)
+            break
+          case "doughnut":
+            initdoughnut(dataToTreat, metadata, box)
+            break
+          case "horizontalBar":
+            initHorizontalBar(dataToTreat, metadata, box)
+            break
+          default:
+            d3.selectAll("svg").remove()
+            d3.selectAll(".chart>p").remove()
 
-                d3.select("'#" + box + "'")
-                    .append('p')
-                    .text("Veuillez séléctionner un type de graphe avant de continuer.")
+            d3.select("'#" + box + "'")
+            .append('p')
+            .text("Veuillez séléctionner un type de graphe avant de continuer.")
         }
+      }
+      onChange()
     }
-    onChange()
 }
