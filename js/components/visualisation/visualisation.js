b4a4f4286db28613a10699a9403a9c8164a8f6cb
Calandre <jordan.calandre@etudiant.univ-lr.fr>
2018-03-12 19:57:10 +0100

    fix : bug when changing the date

--- js/components/visualisation/visualisation.js
+++ js/components/visualisation/visualisation.js
@@ -1,13 +1,11 @@
+$(document).ready(function () {
 
-
-$(document).ready(function(){
-
-	/**
-	 * Init
-	 *
-	 * @return
-	 */
-	function init() {
+    /**
+     * Init
+     *
+     * @return
+     */
+    function init() {
         var result = getUrlPage();
         var typeVisualisation = result[0];
         var nomDonnee = result[1];
@@ -15,7 +13,7 @@ $(document).ready(function(){
         if (typeVisualisation && nomDonnee) {
 
             // Get metadata
-            getMetadata(nomDonnee, function(metadata) {
+            getMetadata(nomDonnee, function (metadata) {
 
                 // Set title
                 setTitle(metadata.title);
@@ -24,7 +22,7 @@ $(document).ready(function(){
                 setActivePanel(typeVisualisation);
 
                 // With end url, get data
-                getData(metadata.link, function(data) {
+                getData(metadata.link, function (data) {
 
                     // Set tab navbar and draw visualisation
                     setTabNavBarAndDraw(metadata, data);
@@ -33,48 +31,48 @@ $(document).ready(function(){
                 setMapButton();
             });
         }
-	};
+    };
 
-	/**
-	 * Init listeners
-	 *
-	 * @return
-	 */
-	var bindListeners = function() {
+    /**
+     * Init listeners
+     *
+     * @return
+     */
+    var bindListeners = function () {
         // Oui c'est sale ... chuuuuuut ... si vous avez mieux à proposer GO!
-        $(".tab-nav").click(function() {
+        $(".tab-nav").click(function () {
             $(".tab-pane").css('display', 'none');
             $(".tab-nav").removeClass('active');
         });
 
-        $("#tab-nav-1").click(function() {
+        $("#tab-nav-1").click(function () {
             $('#tab-pane-1').css('display', 'block');
             $("#tab-nav-1").addClass('active');
         });
 
-        $("#tab-nav-2").click(function() {
+        $("#tab-nav-2").click(function () {
             $('#tab-pane-2').css('display', 'block');
             $("#tab-nav-2").addClass('active');
         });
 
-        $("#tab-nav-3").click(function() {
+        $("#tab-nav-3").click(function () {
             $('#tab-pane-3').css('display', 'block');
             $("#tab-nav-3").addClass('active');
             showMap();
         });
 
-        $("#tab-nav-4").click(function() {
+        $("#tab-nav-4").click(function () {
             $("#tab-pane-4").css('display', 'block');
             $("#tab-nav-4").addClass('active');
         });
 
-        $("#tab-nav-5").click(function() {
+        $("#tab-nav-5").click(function () {
             $("#tab-pane-5").css('display', 'block');
             $("#tab-nav-5").addClass('active');
         });
 
         // Reload the page with the new date
-        $(".select-list-date").change(function() {
+        $(".select-list-date").change(function () {
             // Get value of the selected item 
             var nomDonnee = $('.select-list-date :selected').val();
             var url = window.location.href.split("?");
@@ -83,19 +81,19 @@ $(document).ready(function(){
         });
 
         // Load the compare table
-        $(".select-list-date-compare").change(function() {
+        $(".select-list-date-compare").change(function () {
             // Get value of the selected item 
             var nomDonnee = $("option:selected", this).val();
             var typeVisualisation = getActivePanel();
-            
+
             if (nomDonnee == "none") {
                 removeDrawCompare(typeVisualisation);
             } else {
 
                 // Get metadata
-                getMetadata(nomDonnee, function(metadata) {
+                getMetadata(nomDonnee, function (metadata) {
                     // With end url, get data
-                    getData(metadata.link, function(data) {
+                    getData(metadata.link, function (data) {
                         if (data) {
                             // Draw visualisation
                             drawCompare(typeVisualisation, metadata, data);
@@ -106,14 +104,14 @@ $(document).ready(function(){
                 });
             }
         });
-	};
+    };
 
     /**
      * Get the panel active
      *
-     * @return type if visualisation 
+     * @return type if visualisation
      */
-    var getActivePanel = function() {
+    var getActivePanel = function () {
         if ($("#tab-nav-1").hasClass("active")) {
             return "table";
         } else if ($("#tab-nav-2").hasClass("active")) {
@@ -128,15 +126,15 @@ $(document).ready(function(){
      *
      * @return null
      */
-    var drawCompare = function(typeVisualisation, metadata, data) {
+    var drawCompare = function (typeVisualisation, metadata, data) {
 
         switch (typeVisualisation) {
             case 'table':
                 // Remove the table compare if exists
                 if ($("#my_table_box1Compare_wrapper").length) {
                     $("#my_table_box1Compare_wrapper").remove();
-                
-                // Else, create div compare
+
+                    // Else, create div compare
                 } else {
                     var div = document.createElement('div');
                     div.id = 'box1Compare';
@@ -151,10 +149,10 @@ $(document).ready(function(){
             case 'graph':
 
                 // Remove the table compare if exists
-                if ($("#box2Compare").length) {
-                    $("#box2Compare").remove();
-                
-                // Else, create div compare
+                if ($("#chartbox2Compare").length) {
+                    $("#chartbox2Compare").remove();
+
+                    // Else, create div compare
                 } else {
                     var div = document.createElement('div');
                     div.id = 'box2Compare';
@@ -171,7 +169,7 @@ $(document).ready(function(){
      *
      * @return null
      */
-    var removeDrawCompare = function(typeVisualisation) {
+    var removeDrawCompare = function (typeVisualisation) {
         switch (typeVisualisation) {
             case 'table':
                 // Remove the table compare
@@ -182,12 +180,12 @@ $(document).ready(function(){
 
             case 'graph':
                 // Remove the graph compare
-                if ($("#box2Compare").length) {
+                if ($("#chartbox2Compare").length) {
                     $("#box2Compare").remove();
                 }
                 break;
         }
-        
+
     };
 
     /**
@@ -195,35 +193,35 @@ $(document).ready(function(){
      *
      * @return
      */
-    var setMapButton = function() {
-        $(".button-icone").on("click", function() {
+    var setMapButton = function () {
+        $(".button-icone").on("click", function () {
             // If the marks are visible, hide them
             if ($(this).val() === "true") {
                 $(this).val("false");
                 $(this).css({opacity: 0.5});
 
                 // Remove bus marks
-                if($(this).attr('id') === "button-icone-bus") {
+                if ($(this).attr('id') === "button-icone-bus") {
                     hideLayerBus();
                 }
 
                 // Remove poste marks
-                if($(this).attr('id') === "button-icone-poste") {
+                if ($(this).attr('id') === "button-icone-poste") {
                     hideLayerPoste();
                 }
 
-            // Else, show them
+                // Else, show them
             } else {
                 $(this).val("true");
                 $(this).css({opacity: 1});
 
                 // Draw bus marks
-                if($(this).attr('id') === "button-icone-bus") {
+                if ($(this).attr('id') === "button-icone-bus") {
                     showLayerBus();
                 }
 
                 // Draw poste marks
-                if($(this).attr('id') === "button-icone-poste") {
+                if ($(this).attr('id') === "button-icone-poste") {
                     showLayerPoste();
                 }
             }
@@ -235,7 +233,7 @@ $(document).ready(function(){
      *
      * @return
      */
-    var setTitle = function(title) {
+    var setTitle = function (title) {
         $("#title-page").html(title);
     };
 
@@ -244,7 +242,7 @@ $(document).ready(function(){
      *
      * @return
      */
-    var setDescription = function(metadata) {
+    var setDescription = function (metadata) {
         $("#description-page").html(metadata.description);
     };
 
@@ -253,7 +251,7 @@ $(document).ready(function(){
      *
      * @return
      */
-    var setActivePanel = function(typeVisualisation) {
+    var setActivePanel = function (typeVisualisation) {
         switch (typeVisualisation) {
             case 'table':
                 $('#tab-pane-1').css('display', 'block');
@@ -280,29 +278,29 @@ $(document).ready(function(){
     /**
      * Set the tab navbar
      *
-     * @return type if visualisation 
+     * @return type if visualisation
      */
-    var setTabNavBarAndDraw = function(metadata, data) {
+    var setTabNavBarAndDraw = function (metadata, data) {
 
-        if(metadata.table) {
+        if (metadata.table) {
             drawTable(data, metadata, 'box1');
         } else {
             $("#tab-nav-1").css('display', 'none');
         }
 
-        if(metadata.graph) {
+        if (metadata.graph) {
             drawGraph(data, metadata, 'box2');
         } else {
             $("#tab-nav-2").css('display', 'none');
         }
 
-        if(metadata.map) {
+        if (metadata.map) {
             drawMap(data, metadata, 'box3', 'popup');
         } else {
             $("#tab-nav-3").css('display', 'none');
         }
 
-        if(metadata.timeline) {
+        if (metadata.timeline) {
             // Draw timeline
             $('.select-list-date').append("<p class='select-list select-list-text'>Choisissez l'année : </p>");
             var sel = $('<select>').appendTo('.select-list-date');
@@ -314,12 +312,12 @@ $(document).ready(function(){
             var selCompare = $('<select>').appendTo('.select-list-date-compare');
             selCompare.addClass("select-list");
             selCompare.addClass("select-list-select");
-            selCompare.append($("<option>").attr('value','none').text('...'));
+            selCompare.append($("<option>").attr('value', 'none').text('...'));
             selCompare.addClass("form-control");
 
             for (var key in metadata.timeline.dates) {
-                sel.append($("<option>").attr('value',metadata.timeline.dates[key]).text(key));
-                selCompare.append($("<option>").attr('value',metadata.timeline.dates[key]).text(key));
+                sel.append($("<option>").attr('value', metadata.timeline.dates[key]).text(key));
+                selCompare.append($("<option>").attr('value', metadata.timeline.dates[key]).text(key));
             }
 
             // Set active select
@@ -330,6 +328,6 @@ $(document).ready(function(){
         }
     };
 
-	init();
-	bindListeners();
+    init();
+    bindListeners();
 });
