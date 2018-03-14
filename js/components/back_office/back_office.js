6fc59b35fb087714f447747c5d4b8b2437ab6b3d
Calandre <jordan.calandre@etudiant.univ-lr.fr>
2018-03-14 15:04:10 +0100

    clear spaces back_office

--- js/components/back_office/back_office.js
+++ js/components/back_office/back_office.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	// var addInputTable = 0;
-	// var addInputTimeline = 0;
+$(document).ready(function () {
+    // var addInputTable = 0;
+    // var addInputTimeline = 0;
 
-	//Add the input for the table :
-	$("#b1").click(function() {
-    		var lastField = $("#buildyourform div:last");
+    //Add the input for the table :
+    $("#b1").click(function () {
+        var lastField = $("#buildyourform div:last");
         var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
         var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
         fieldWrapper.data("idx", intId);
         var fName = $("<h6>Mot clé / valeur</h6><input type=\"text\" class=\"input form-control\" name=\"table_dataComposition_keys_list[]\" placeholder=\"Mot clé\" required/><input type=\"text\" class=\"input form-control\" name=\"table_dataComposition_value_list[]\" placeholder=\"Valeur\" required/>");
         var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
-        removeButton.click(function() {
+        removeButton.click(function () {
             $(this).parent().remove();
         });
         fieldWrapper.append(fName);
@@ -18,99 +18,99 @@ $(document).ready(function(){
         $("#buildyourform").append(fieldWrapper);
     });
 
-		//Add the input for the timeline :
-		$("#b_timeline").click(function() {
-					var lastField = $("#buildtimeline div:last");
-					var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
-					var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
-					fieldWrapper.data("idx", intId);
-					var fName = $("<h6>Année / valeur</h6><input type=\"text\" class=\"input form-control\" name=\"timeline_dataComposition_year_list[]\" placeholder=\"Année\" required/><input type=\"text\" class=\"input form-control\" name=\"timeline_dataComposition_year_value_list[]\" placeholder=\"Valeur de l'année\" required/>");
-					var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
-					removeButton.click(function() {
-							$(this).parent().remove();
-					});
-					fieldWrapper.append(fName);
-					fieldWrapper.append(removeButton);
-					$("#buildtimeline").append(fieldWrapper);
-			});
+    //Add the input for the timeline :
+    $("#b_timeline").click(function () {
+        var lastField = $("#buildtimeline div:last");
+        var intId = (lastField && lastField.length && lastField.data("idx") + 1) || 1;
+        var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
+        fieldWrapper.data("idx", intId);
+        var fName = $("<h6>Année / valeur</h6><input type=\"text\" class=\"input form-control\" name=\"timeline_dataComposition_year_list[]\" placeholder=\"Année\" required/><input type=\"text\" class=\"input form-control\" name=\"timeline_dataComposition_year_value_list[]\" placeholder=\"Valeur de l'année\" required/>");
+        var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
+        removeButton.click(function () {
+            $(this).parent().remove();
+        });
+        fieldWrapper.append(fName);
+        fieldWrapper.append(removeButton);
+        $("#buildtimeline").append(fieldWrapper);
+    });
 
 
-	function init() {
-	}
+    function init() {
+    }
 
- 	init();
+    init();
 });
 
 
 function atleast_onecheckbox(e) {
-  if ($("input[type=checkbox]:checked").length === 0) {
-      e.preventDefault();
-      alert('Il faut choisir au moins un type de visualisation !');
-      return false;
-  }
-  if(document.getElementById('type_graphe').checked){
-		var inputs = document.getElementById('theGraph').getElementsByTagName('input');
-		var counter = 0;
-		for (var i = 0 ; i < inputs.length ; i++){
-			if(inputs[i].checked){
-				counter ++;
-			}
-		}
-		if(counter == 0){
-			e.preventDefault();
-			alert('Il faut choisir au moins un type de graphe !');
-			return false;
+    if ($("input[type=checkbox]:checked").length === 0) {
+        e.preventDefault();
+        alert('Il faut choisir au moins un type de visualisation !');
+        return false;
+    }
+    if (document.getElementById('type_graphe').checked) {
+        var inputs = document.getElementById('theGraph').getElementsByTagName('input');
+        var counter = 0;
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked) {
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            e.preventDefault();
+            alert('Il faut choisir au moins un type de graphe !');
+            return false;
 
-		}
-	}
+        }
+    }
 }
 
 var id = 0;
 var next = 1;
 
-function requiredInputs(){
-	$("#theGraph :input.form-control").prop('required',true);
+function requiredInputs() {
+    $("#theGraph :input.form-control").prop('required', true);
 
 }
 
 function dataTypeChoice() {
     if (document.getElementById('type_map').checked) {
         document.getElementById('theMap').style.display = 'block';
-				$("#theMap :input.form-control").prop('required',true);
+        $("#theMap :input.form-control").prop('required', true);
 
     }
-		if (!document.getElementById('type_map').checked) {
-				document.getElementById('theMap').style.display = 'none';
-				$("#theMap :input.form-control").prop('required',false);
+    if (!document.getElementById('type_map').checked) {
+        document.getElementById('theMap').style.display = 'none';
+        $("#theMap :input.form-control").prop('required', false);
 
-		}
+    }
 
-    if(document.getElementById('type_graphe').checked){
+    if (document.getElementById('type_graphe').checked) {
         document.getElementById('theGraph').style.display = 'block';
-				$("#theGraph :input.form-control").prop('required',true);
-     }
-		if(!document.getElementById('type_graphe').checked){
- 				document.getElementById('theGraph').style.display = 'none';
-				$("#theGraph :input.form-control").prop('required',false);
- 		 }
+        $("#theGraph :input.form-control").prop('required', true);
+    }
+    if (!document.getElementById('type_graphe').checked) {
+        document.getElementById('theGraph').style.display = 'none';
+        $("#theGraph :input.form-control").prop('required', false);
+    }
 
-   	if(document.getElementById('type_table').checked){
+    if (document.getElementById('type_table').checked) {
         document.getElementById('theTable').style.display = 'block';
-				$("#theTable :input.form-control").prop('required',true);
-     }
-  	if(!document.getElementById('type_table').checked){
-         document.getElementById('theTable').style.display = 'none';
-				 $("#theTable :input.form-control").prop('required',false);
+        $("#theTable :input.form-control").prop('required', true);
+    }
+    if (!document.getElementById('type_table').checked) {
+        document.getElementById('theTable').style.display = 'none';
+        $("#theTable :input.form-control").prop('required', false);
     }
 
-		if (document.getElementById('type_timeline').checked) {
+    if (document.getElementById('type_timeline').checked) {
         document.getElementById('theTimeline').style.display = 'block';
-				$("#theTimeline :input.form-control").prop('required',true);
+        $("#theTimeline :input.form-control").prop('required', true);
     }
-		if (!document.getElementById('type_timeline').checked) {
-				document.getElementById('theTimeline').style.display = 'none';
-				$("#theTimeline :input.form-control").prop('required',false);
-		}
+    if (!document.getElementById('type_timeline').checked) {
+        document.getElementById('theTimeline').style.display = 'none';
+        $("#theTimeline :input.form-control").prop('required', false);
+    }
 
 
 }
@@ -118,28 +118,28 @@ function dataTypeChoice() {
 function mapChoice() {
     if (document.getElementById('map_newMap').checked) {
         document.getElementById('new_map').style.display = 'block';
-				document.getElementById('kmlFile_map').style.display = 'none';
-				$("#new_map :input.form-control").prop('required',true);
-				$("#kmlFile_map :input.form-control").prop('required',false);
+        document.getElementById('kmlFile_map').style.display = 'none';
+        $("#new_map :input.form-control").prop('required', true);
+        $("#kmlFile_map :input.form-control").prop('required', false);
 
 
-     }
-  	else if(document.getElementById('map_fileKml').checked){
+    }
+    else if (document.getElementById('map_fileKml').checked) {
         document.getElementById('kmlFile_map').style.display = 'block';
-				document.getElementById('new_map').style.display = 'none';
-				$("#kmlFile_map :input.form-control").prop('required',true);
-				$("#new_map :input.form-control").prop('required',false);
+        document.getElementById('new_map').style.display = 'none';
+        $("#kmlFile_map :input.form-control").prop('required', true);
+        $("#new_map :input.form-control").prop('required', false);
 
-     }
+    }
 }
 
 function addDictionary() {
     if (document.getElementById('add_dict').checked) {
         document.getElementById('theDictionary').style.display = 'block';
-				$("#theDictionary :input.form-control").prop('required',true);
-     }
-  	else if(!document.getElementById('map_fileKml').checked){
-				document.getElementById('theDictionary').style.display = 'none';
-				$("#theDictionary :input.form-control").prop('required',false);
-     }
+        $("#theDictionary :input.form-control").prop('required', true);
+    }
+    else if (!document.getElementById('map_fileKml').checked) {
+        document.getElementById('theDictionary').style.display = 'none';
+        $("#theDictionary :input.form-control").prop('required', false);
+    }
 }
