c9331d93026c3f47bf55f41bd28fb156a898bf78
Calandre <jordan.calandre@etudiant.univ-lr.fr>
2018-03-14 15:03:47 +0100

    clear spaces donnees

--- js/components/donnees/donnees.js
+++ js/components/donnees/donnees.js
@@ -1,25 +1,23 @@
+$(document).ready(function () {
 
+    /**
+     * Init
+     *
+     * @return
+     */
+    function init() {
 
-$(document).ready(function(){
+    };
 
-	/**
-	 * Init
-	 *
-	 * @return
-	 */
-	function init() {
-		
-	};
+    /**
+     * Add bind listeners
+     *
+     * @return
+     */
+    var bindListeners = function () {
 
-	/**
-	 * Add bind listeners
-	 *
-	 * @return
-	 */
-	var bindListeners = function() {
-
-	};
+    };
 
     init();
-	bindListeners();
+    bindListeners();
 });
