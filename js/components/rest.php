ac487f18a70566a87813fc22cff92c2e46be487e
quentinnicolle <qnicolle@me.com>
2018-04-05 00:20:05 +0200

    :insecte: comment proxy

--- js/components/rest.php
+++ js/components/rest.php
@@ -13,7 +13,7 @@
         //$proxy = 'wwwcache.univ-lr.fr:3128';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        //curl_setopt($ch, CURLOPT_PROXY, $proxy);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         //curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
