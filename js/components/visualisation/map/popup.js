322b377c39d5291d0510203790d60f66899e0d81
Calandre <jordan.calandre@etudiant.univ-lr.fr>
2018-04-04 22:57:15 +0200

    kml data => popup are working

--- js/components/visualisation/map/popup.js
+++ js/components/visualisation/map/popup.js
@@ -18,40 +18,41 @@ function addPopup(map, metadata, myPopup, description) {
     // display popup on click
     map.on('click', function (evt) {
         // Si KML : On ne clique pas sur une zone
-        if (!metadata.map.kml) {
-            // Extraction de la zone cliquee
-            var feature = map.forEachFeatureAtPixel(evt.pixel,
-                function (feature, layer) {
-                    return feature;
-                });
-            if (feature) {
-                // Affichage du marqueur selon son type
-                if (feature.get('type') && feature.get('type') != "openData") {
+        // Extraction de la zone cliquee
+        var feature = map.forEachFeatureAtPixel(evt.pixel,
+            function (feature, layer) {
+                return feature;
+            });
+        if (feature) {
+            // Affichage du marqueur selon son type
+            if (feature.get('type') && feature.get('type') != "openData") {
+                var geometry = feature.getGeometry();
+                var coord = geometry.getCoordinates();
+                popup.setPosition(coord);
+                $(element).attr('data-placement', 'top');
+                $(element).attr('data-html', true);
+                $(element).attr('data-original-title', '<p>' + feature.get('name') + '</p>');
+                $(element).attr('data-content', feature.get('type'));
+                $(element).popover('show');
+            }
+            else {
+                if (feature.get("dispo")) {
                     var geometry = feature.getGeometry();
                     var coord = geometry.getCoordinates();
                     popup.setPosition(coord);
                     $(element).attr('data-placement', 'top');
                     $(element).attr('data-html', true);
-                    $(element).attr('data-original-title', '<p>' + feature.get('name') + '</p>');
-                    $(element).attr('data-content', feature.get('type'));
+                    $(element).attr('data-content', '<p>' + description + ' </p><code>' + feature.get('dispo') + '/' + feature.get('total') + '</code>');
+                    $(element).attr('data-original-title', feature.get('name'));
                     $(element).popover('show');
                 }
                 else {
-                    if (feature.get("dispo")) {
-                        var geometry = feature.getGeometry();
-                        var coord = geometry.getCoordinates();
-                        popup.setPosition(coord);
-                        $(element).attr('data-placement', 'top');
-                        $(element).attr('data-html', true);
-                        $(element).attr('data-content', '<p>' + description + ' </p><code>' + feature.get('dispo') + '/' + feature.get('total') + '</code>');
-                        $(element).attr('data-original-title', feature.get('name'));
-                        $(element).popover('show');
-                    }
+                    $(element).popover('destroy');
                 }
             }
-            else {
-                $(element).popover('destroy');
-            }
+        }
+        else {
+            $(element).popover('destroy');
         }
     });
 }
